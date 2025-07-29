d543b88908f7414f81d33b6d2f05cca9eb8b1646
DhrithiObla <dhrithiobla@gmail.com>
2025-07-29 09:17:06 -0400

    app.py.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with tab1:
        st.markdown("[Fermilab ES&H, 4240TA-11-15](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/Cryogen%20Safety/Fermi%20Lab.pdf?csf=1&web=1&e=aimldu)") 
 
     with col2:
-        st.markdown("[SLAC 2009, Page 5-7 of 9](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/Failure%20%20Release%20Rates/SLAC%20Feb%202009.pdf?csf=1&web=1&e=BdjqiF)")
+        st.markdown("[SLAC 2009, Pages 5-7 of 9](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/Failure%20%20Release%20Rates/SLAC%20Feb%202009.pdf?csf=1&web=1&e=BdjqiF)")
    
     with col3:
         st.markdown("[ESS-0038692, 14-16(17)](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/ESS-0038692.pdf?csf=1&web=1&e=uerZNT)")
@@ -28,7 +28,7 @@ with tab1:
         st.markdown("[L. C. Cadwallader, 15](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/Failure%20%20Release%20Rates/SELECTED%20COMPONENT%20FAILURE%20RATE%20VALUES.pdf?csf=1&web=1&e=SOOeXC)")
 
     with col6:
-        st.markdown("[Roymech, Page 1-2 of 2](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/Failure%20Rates%20Roymech.co.uk.pdf?csf=1&web=1&e=6Ri7zP)")
+        st.markdown("[Roymech, Pages 1-2 of 2](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/ODH%20and%20LEL%20Analysis/Failure%20Rates%20Roymech.co.uk.pdf?csf=1&web=1&e=6Ri7zP)")
 
     search_term = st.text_input("Search across all tables:")
 
@@ -75,7 +75,7 @@ with tab2:
 
     st.subheader("Sources")
 
-    col1, col2, col3, col4, col5, col6, col7= st.columns(7)
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 
     with col1:
        st.markdown("[Kingston Model 115, Page 2](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-Kingston-Model_115.pdf)") 
@@ -87,16 +87,16 @@ with tab2:
         st.markdown("[Kingston Model 112, Page 2](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-Kingston-112CSS.pdf)")
 
     with col4:
-        st.markdown("[Kunkle Model 30, Page 2](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-Kunkle-_Model_30.pdf)")
-
-    with col5:
         st.markdown("[Kingston Model 119/118, Page 2](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-Kingston-119CSS.pdf)")
 
-    with col6:
+    with col5:
         st.markdown("[Kingston Model KSV12 & KSV25, Page 2](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-Kingston-KSV12__KSV25.pdf)")
 
-    with col7:
+    with col6:
         st.markdown("[Kunkle Models 6933, 6934, 6935, Page 5](https://lqms.gsfc.nasa.gov/lqms-media/pvs/compfile/RV-KUNKLE-Series_6000.pdf)")
+
+    with col7:
+        st.markdown("[NFPA 55 2005 Edition, 55-24](https://nasa.sharepoint.com/:b:/r/teams/IHTeam/Shared%20Documents/Standards,%20Program%20%26%20Policies/NFPA%2055.pdf?csf=1&web=1&e=TbcFqD)")
 
     st.subheader("Not a Relief Valve?")
     st.markdown("[Link to LQMS](https://lqms.gsfc.nasa.gov)")
@@ -105,7 +105,7 @@ with tab2:
 
     files = [
         "Kingston Model 115.csv", "Kunkle Models 541, 542, & 548.csv", "Kingston Model 112.csv", "Kunkle Model 30.csv",
-        "Kingston Model 119:118.csv", "Kingston Model KSV12 &KSV25.csv", "Kunkle Models 6933, 6934, 6935.csv"
+        "Kingston Model 119:118.csv", "Kingston Model KSV12 &KSV25.csv", "Kunkle Models 6933, 6934, 6935.csv", "NFPA 55 2005 Edition.csv"
     ]
 
     for file in files:
